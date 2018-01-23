de3b5c581cdaee70def1feae5adbbf77a6e9a59f
arch-DJ <divyanshjamuaar@gmail.com>
2018-01-23 18:11:00 +0530

    Update teacher registration activity

--- app/src/main/java/com/slashroot/studentmobileapp/MainActivity.java
+++ app/src/main/java/com/slashroot/studentmobileapp/MainActivity.java
@@ -133,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
 
         else if (sharedPreferences.getString("userType", "").equals("teacher")) {
             Intent intent = new Intent(getApplicationContext(), TeacherMainActivity.class);
-            Toast.makeText(this, "Welcome " + sharedPreferences.getString("name", "") + "!", Toast.LENGTH_SHORT).show();
             startActivity(intent);
             finish();
         }
@@ -144,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             //startActivity(intent);
             finish();
         }
+
     }
 
     public class ServerConnect extends AsyncTask<String, Void, String> {
@@ -286,6 +286,22 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                     case "teacher": {
                                         Intent intent = new Intent(getApplicationContext(), TeacherMainActivity.class);
+                                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("UserDetails", 0);
+                                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        editor.putString("userType", userType);
+                                        editor.putString("name", json.getString("name"));
+                                        editor.putString("userName", json.getString("userName"));
+                                        editor.putString("gender", json.getString("gender"));
+                                        editor.putString("dob", json.getString("dob"));
+                                        editor.putString("college", json.getString("college"));
+                                        editor.putString("department", json.getString("department"));
+                                        editor.putString("address", json.getString("address"));
+                                        editor.putString("email", json.getString("email"));
+                                        editor.putString("aadhaar", json.getString("aadhaar"));
+                                        editor.putString("phone", json.getString("phone"));
+                                        editor.putString("stream", json.getString("stream"));
+                                        editor.putString("university", json.getString("university"));
+                                        editor.apply();
                                         startActivity(intent);
                                         finish();
                                         break;
