1ff8590a4e77ab1ab49f9d5118f45ef81a6e3dfc
arch-DJ <divyanshjamuaar@gmail.com>
2018-01-26 20:19:30 +0530

    Create tab layout for Student interface

---
+++ app/src/main/java/com/slashroot/studentmobileapp/StudentMainFragment.java
@@ -0,0 +1,20 @@
+package com.slashroot.studentmobileapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by divyansh on 1/26/18.
+ */
+
+public class StudentMainFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.student_main_fragment,container,false);
+        return view;
+    }
+}
